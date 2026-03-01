1e2d2e0b42402e67cb9edcb26ff995c304ff9769
Kate_nguyen <253353253+MeoMeoDestroyer@users.noreply.github.com>
2026-02-28 22:01:18 -0800

    finally done, EJS added and update all requirement

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 // Import the express module
-
 import express from 'express';
 
-
 // Create an instance of an Express application
-
 const app = express();
-
 
 // Define the port number where our server will listen
 
@@ -15,23 +11,29 @@ const PORT = 3004;
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+//set ejs as the view engine
+app.set('view engine', 'ejs');
+
 // Define a default "route" ('/')
 app.use(express.static('public'));
 
 const submissions = [];
 
-// home.html
+// home.html is the resume 
 app.get('/', (req, res) => {
-
-  res.sendFile(`${import.meta.dirname}/views/home.html`);
-
+  res.render(`${import.meta.dirname}/views/home.ejs`);
+});
+//contact-form is the form page
+app.get('/contact-form', (req, res) => {
+  res.render(`${import.meta.dirname}/views/contact-form.ejs`);
 });
 // Confirmation.html
 app.get('/confirmation', (req, res) => {
-  res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+  res.render(`${import.meta.dirname}/views/confirmation.ejs`);
 });
 app.post('/submit', (req, res) => {
-  // Create submission object
   const submission = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -46,20 +48,18 @@ app.post('/submit', (req, res) => {
     emailFormat: req.body.emailFormat,
     timestamp: new Date()
   };
-
   submissions.push(submission);
-  console.log('Form data received:', submission);
-    res.redirect('/confirmation');
+  console.log('Form received:', submission);
+    res.render(`${import.meta.dirname}/views/confirmation.ejs`, {submission});
 });
-  app.get('/admin', (req, res) => {
-  res.json(submissions);
+//admin routes
+app.get('/admin', (req, res) => {
+    res.render('admin', {submissions: submissions});
 });
 // Start the server and listen on the specified port
 
   
 app.listen(PORT, () => {
-
     console.log(`Server is running at http://localhost:${PORT}`);
-
 
 });
